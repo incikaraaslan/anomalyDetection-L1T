aa6137570fe2d3f9a269d30d4d4173cd288e0be9
incikaraaslan <incikaraaslan@gmail.com>
2023-06-09 10:34:09 -0500

    MLtest

--- genTrainML/MLtest.py
+++ genTrainML/MLtest.py
@@ -2,6 +2,13 @@ import h5py
 import numpy as np
 
 # Read files from Andrew's large file
-directory = 
-with h5py.File('random.hdf5', 'w') as f:
-    dset = f.create_dataset("default", data=arr)
+directory = '/hdfs/store/user/aloeliger/largeInputFile_manyInput_CICADAv2.hdf5'
+with h5py.File(directory, 'r') as f:
+    cicadaInput = f['cicadaInput']
+    pileup = f['pileup']
+    print(cicadaInput[:10])
+    print(pileup[:10])
+    for k in f.keys():
+        print(k)
+
+# It will have 4 datasets inside, but the ones you care about will be called "cicadaInput" and "pileup".
